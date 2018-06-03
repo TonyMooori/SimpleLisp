8b373ba0fe7e122d981c396f39e05dc43e4553c0
TonyMooori <tony.mooori@gmail.com>
2018-06-04 00:28:26 +0900

    add a MalType

--- src/token.rs
+++ src/token.rs
@@ -5,11 +5,23 @@ pub enum TokenKind{
     Integer(i64), 
     Symbol(String),     // [],(),{},`,',@,~,~@,^
     Str(String),
-
 }
 
+#[derive(PartialEq, Debug, Clone)]
 pub struct Token{
     pub kind : TokenKind,
     pub start : usize,
     pub end : usize,
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum MalType{
+    Identifier(String),
+    Integer(i64),
+    Str(String),
+    Bool(bool),
+    Vector(Vec<MalType>),
+    List(Vec<MalType>),
+    Function(Vec<MalType>),
+    Nil,
 }
