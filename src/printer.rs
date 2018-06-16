62f912bfc0f6b732301d0f25cb52e95a139fa7a0
TonyMooori <tony.mooori@gmail.com>
2018-06-17 06:35:09 +0900

    implemented map

--- src/printer.rs
+++ src/printer.rs
@@ -8,16 +8,4 @@ impl Interpreter{
             Err(e) => println!("{}",e),
         }
     }
-
-    // STEP 1: Defferrable 1
-    // pub fn to_readable(&self:mt:MalType) -> String{
-    //     match mt{
-    //         MalType::Identifier(s) => s,
-    //         MalType::Integer(n) => format!("{}",n),
-    //         MalType::Str(s) => s, // TODO: "\n"
-    //         MalType::Bool(f) => format!("{}",f),
-    //         MalType::Vector(v) => {
-    //         }
-    //     }
-    // }
 }
